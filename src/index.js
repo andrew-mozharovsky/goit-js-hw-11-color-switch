2431b496b3e21fa4127307529e7f41916ab841fa
Andrew Mozharovsky <kainnet.job@gmail.com>
2021-04-09 18:59:06 +0300

    09-04

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/style.css'
+import refs from './js/refs'
+import {startButton, stopButton} from './js/actionButtons'
+
+
+refs.start.addEventListener('click', startButton)
+refs.stop.addEventListener('click', stopButton)
+
+
+
+
+  
